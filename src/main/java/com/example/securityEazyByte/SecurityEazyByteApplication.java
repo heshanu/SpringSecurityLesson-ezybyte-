625481fd27d80472b389f036913b1121d9390c5b
heshanu <heshanu97@gmail.com>
2022-08-06 03:46:15 +0530

    init security

---
+++ src/main/java/com/example/securityEazyByte/SecurityEazyByteApplication.java
@@ -0,0 +1,13 @@
+package com.example.securityEazyByte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityEazyByteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityEazyByteApplication.class, args);
+	}
+
+}
