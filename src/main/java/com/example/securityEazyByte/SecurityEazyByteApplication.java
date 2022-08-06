291e21657980a8a80bc8ed1f0e95487f65acd231
heshanu <heshanu97@gmail.com>
2022-08-06 05:48:32 +0530

    added changes

--- src/main/java/com/example/securityEazyByte/SecurityEazyByteApplication.java
+++ src/main/java/com/example/securityEazyByte/SecurityEazyByteApplication.java
@@ -2,8 +2,14 @@ package com.example.securityEazyByte;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 
 @SpringBootApplication
+@ComponentScans({
+		@ComponentScan("com.example.securityEazyByte.controller"),
+		@ComponentScan("com.example.securityEazyByte.config")
+})
 public class SecurityEazyByteApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/example/securityEazyByte/controller/NoticesController.java
@@ -0,0 +1,15 @@
+package com.example.securityEazyByte.controller;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NoticesController {
+
+	@GetMapping("/notices")
+	public String getNotices(String input) {
+		return "Here are the notices details from the DB";
+	}
+
+}
