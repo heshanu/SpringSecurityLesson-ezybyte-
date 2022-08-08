c6ea3e2a024dfaef12ebe807889f36664adfeba9
heshanu <heshanu97@gmail.com>
2022-08-08 07:47:10 +0530

    added changes in secu auth using inmemoryAuthentication

--- src/main/java/com/example/securityEazyByte/config/ProjectSecurityConfig.java
+++ src/main/java/com/example/securityEazyByte/config/ProjectSecurityConfig.java
@@ -1,9 +1,13 @@
 package com.example.securityEazyByte.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -35,10 +39,10 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/myBalance").authenticated()
                 .antMatchers("/myLoans").authenticated()
                 .antMatchers("/myCards").authenticated()
-                .antMatchers("/**").denyAll()
                 .antMatchers("/notices").permitAll()
-                .antMatchers("/contact").permitAll().and()
-                .formLogin().and().httpBasic();
+                .antMatchers("/contact").permitAll()
+                .and().formLogin()
+                .and().httpBasic();
 
 
         /**
@@ -60,5 +64,17 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
 		*/
 
     }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("admin").password("123").authorities("admin")
+                .and()
+                .withUser("user").password("123").authorities("read")
+                .and()
+                .passwordEncoder(NoOpPasswordEncoder.getInstance());
+    }
+
 
 }
